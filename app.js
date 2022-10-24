082803ebd2f913f18b038484bd635d63b1f0cc90
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-10-25 02:51:13 +0800

    Add default port

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 })
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.m5ycrum.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(() => {
-    app.listen(8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log('listening at port 8000...')
     });
     

e47c00299d7c01f8028f27c36051d068f3619c00
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-10-28 16:59:29 +0800

    Starter file for cd

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app');
+const app = require('../server');
 const { expect } = require('chai');
 var superagent = require('superagent');
 var agent = superagent.agent();
